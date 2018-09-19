bb5d7beb01f4fa4dec6f2e54f5a344da63fc9d70
Nils Baumgartner <nilsbaumgartner1994@gmail.com>
2018-09-19 09:09:43 +0200

    Added z Component to Position and fixed bugs in Position

--- core/src/com/gentlemansoftware/pixelworld/physics/Position.java
+++ core/src/com/gentlemansoftware/pixelworld/physics/Position.java
@@ -11,18 +11,37 @@ public class Position {
 	 */
 	public int x;
 	public int y;
+	public int z;
 	public int xFraction;
 	public int yFraction;
+	public int zFraction;
 
 	public static final transient int fractionMax_x = MapTile.tileWidth;
 	public static final transient int fractionMax_y = MapTile.tileHeight;
+	public static final transient int fractionMax_z = MapTile.tileHeight;
 
 	static transient int speed = 1;
-	public static transient final Position STOP = new Position(0, 0, 0, 0);
-	public static transient final Position NORTH = new Position(0, 0, 0, speed);
-	public static transient final Position EAST = new Position(0, 2 * speed, 0, 0);
-	public static transient final Position SOUTH = new Position(0, 0, 0, -speed);
-	public static transient final Position WEST = new Position(0, -2 * speed, 0, 0);
+	public static transient final Position STOP = new Position(0, 0, 0, 0, 0, 0);
+	public static transient final Position NORTH = new Position(0, 0, 0, speed, 0, 0);
+	public static transient final Position EAST = new Position(0, 2 * speed, 0, 0, 0, 0);
+	public static transient final Position SOUTH = new Position(0, 0, 0, -speed, 0, 0);
+	public static transient final Position WEST = new Position(0, -2 * speed, 0, 0, 0, 0);
+
+	public Position(int x, int xFraction, int y, int yFraction, int z, int zFraction) {
+		set(x, xFraction, y, yFraction, z, zFraction);
+	}
+
+	public Position(int x, int xFraction, int y, int yFraction) {
+		this(x, xFraction, y, yFraction, 0, 0);
+	}
+
+	public Position(int x, int y) {
+		this(x, 0, y, 0, 0, 0);
+	}
+
+	public Position() {
+		this(0, 0, 0, 0, 0, 0);
+	}
 
 	public static Position getDirection(Position from, Position to) {
 		Position distance = from.distance(to);
@@ -60,13 +79,16 @@ public class Position {
 	public static Position clambIfDistanceToLong(Position from, Position to, Position velocity) {
 		Position distance = to.distance(from);
 		Position back = velocity.cpy();
-		if(velocity.lengthX()>distance.lengthX()){
-			back.set(distance.x, distance.xFraction, velocity.y, velocity.yFraction);
+		if (velocity.lengthX() > distance.lengthX()) {
+			back.set(distance.x, distance.xFraction, velocity.y, velocity.yFraction, velocity.z, velocity.zFraction);
 		}
-		if(velocity.lengthY()>distance.lengthY()){
-			back.set(velocity.x, velocity.xFraction, distance.y, distance.yFraction);
+		if (velocity.lengthY() > distance.lengthY()) {
+			back.set(velocity.x, velocity.xFraction, distance.y, distance.yFraction, velocity.z, velocity.zFraction);
 		}
-		
+		if (velocity.lengthZ() > distance.lengthZ()) {
+			back.set(velocity.x, velocity.xFraction, velocity.y, velocity.yFraction, distance.z, distance.zFraction);
+		}
+
 		return back;
 	}
 
@@ -86,24 +108,14 @@ public class Position {
 		}
 	}
 
-	public Position(int x, int xFraction, int y, int yFraction) {
-		set(x, xFraction, y, yFraction);
-	}
-
-	public Position set(int x, int xFraction, int y, int yFraction) {
+	public Position set(int x, int xFraction, int y, int yFraction, int z, int zFraction) {
 		this.x = x;
 		this.xFraction = xFraction;
 		this.y = y;
 		this.yFraction = yFraction;
+		this.z = z;
+		this.zFraction = zFraction;
 		return calcOverflow();
-	}
-
-	public Position(int x, int y) {
-		this(x, 0, y, 0);
-	}
-
-	public Position() {
-		this(0, 0);
 	}
 
 	public Position calcOverflow() {
@@ -115,11 +127,17 @@ public class Position {
 			yFraction += fractionMax_y;
 			this.y--;
 		}
+		if (zFraction < 0) {
+			zFraction += fractionMax_z;
+			this.z--;
+		}
 
 		this.x = this.x + xFraction / fractionMax_x;
 		this.y = this.y + yFraction / fractionMax_y;
+		this.z = this.z + zFraction / fractionMax_z;
 		this.xFraction = this.xFraction % fractionMax_x;
 		this.yFraction = this.yFraction % fractionMax_y;
+		this.zFraction = this.zFraction % fractionMax_z;
 		return this;
 	}
 
@@ -132,36 +150,42 @@ public class Position {
 	}
 
 	public Position cpy() {
-		return new Position(this.x, this.xFraction, this.y, this.yFraction);
+		return new Position(this.x, this.xFraction, this.y, this.yFraction, this.z, this.zFraction);
 	}
 
+	public Position addAndSet(int x, int xFraction, int y, int yFraction, int z, int zFraction) {
+		return set(this.x + x, this.xFraction + xFraction, this.y + y, this.yFraction + yFraction, this.z + z,
+				this.zFraction + zFraction);
+	}
+	
 	public Position addAndSet(int x, int xFraction, int y, int yFraction) {
-		return set(this.x + x, this.xFraction + xFraction, this.y + y, this.yFraction + yFraction);
+		return addAndSet(x,xFraction,y,yFraction,0,0);
 	}
 
 	public Position addAndSet(Position p) {
-		return addAndSet(p.x, p.xFraction, p.y, p.yFraction);
+		return addAndSet(p.x, p.xFraction, p.y, p.yFraction, p.z, p.zFraction);
 	}
 
 	public Position subAndSet(Position p) {
 		return addAndSet(p.scaleAndSet(-1));
 	}
 
-	public Position scaleAndSet(int scale_x, int scale_y) {
-		return set((this.x * scale_x), (this.xFraction * scale_x), (this.y * scale_x), (this.yFraction * scale_x));
+	public Position scaleAndSet(int scale_x, int scale_y, int scale_z) {
+		return set((this.x * scale_x), (this.xFraction * scale_x), (this.y * scale_y), (this.yFraction * scale_y),
+				(this.z * scale_z), (this.zFraction * scale_z));
 	}
 
 	public Position scaleAndSet(int scale) {
-		return scaleAndSet(scale, scale);
+		return scaleAndSet(scale, scale, scale);
 	}
 
 	public Position scaleAndSet(Position p) {
-		return scaleAndSet(p.x, p.y);
+		return scaleAndSet(p.x, p.y, p.z);
 	}
 
-	public Position scaleAndSet(float scale_x, float scale_y) {
-		return set((int) (this.x * scale_x), (int) (this.xFraction * scale_x), (int) (this.y * scale_x),
-				(int) (this.yFraction * scale_x));
+	public Position scaleAndSet(float scale_x, float scale_y, float scale_z) {
+		return set((int) (this.x * scale_x), (int) (this.xFraction * scale_x), (int) (this.y * scale_y),
+				(int) (this.yFraction * scale_y), (int) (this.z * scale_z), (int) (this.zFraction * scale_z));
 	}
 
 	public Position scaleAndSet(float scale) {
@@ -180,6 +204,10 @@ public class Position {
 
 	public float lengthY() {
 		return Math.abs(this.y + fractionLengthY());
+	}
+
+	public float lengthZ() {
+		return Math.abs(this.z * fractionMax_z + this.zFraction);
 	}
 
 	public float length() {
@@ -206,7 +234,7 @@ public class Position {
 		int xFraction_new = (x_total_new - x_new * divisor) / fractionMax_y;
 		int yFraction_new = (y_total_new - y_new * divisor) / fractionMax_x;
 
-		set(x_new, xFraction_new, y_new, yFraction_new);
+		set(x_new, xFraction_new, y_new, yFraction_new, this.z, this.zFraction);
 		return this;
 	}
 
@@ -234,9 +262,9 @@ public class Position {
 		int comp = getFractionDivisor();
 		return (1.0f * yComp) / comp;
 	}
-	
-	public boolean equals(Position p){
-		return this.x==p.x && this.xFraction == p.xFraction && this.y==p.y && this.yFraction==p.yFraction;
+
+	public boolean equals(Position p) {
+		return this.x == p.x && this.xFraction == p.xFraction && this.y == p.y && this.yFraction == p.yFraction;
 	}
 
 	public String toString() {
