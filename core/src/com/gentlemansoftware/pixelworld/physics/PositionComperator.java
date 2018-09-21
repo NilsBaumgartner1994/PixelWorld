7d8d92e79a7a8fc0babce9c70c04b2e3046cd3b4
Nils Baumgartner <nilsbaumgartner1994@gmail.com>
2018-09-21 08:38:33 +0200

    Fixed Draworder for all Directions

--- core/src/com/gentlemansoftware/pixelworld/physics/PositionComperator.java
+++ core/src/com/gentlemansoftware/pixelworld/physics/PositionComperator.java
@@ -17,22 +17,22 @@ public class PositionComperator implements Comparator<Position> {
 
 	@Override
 	public int compare(Position pos1, Position pos2) {
-		if (this.direction == Direction.NORTH) {
-			return getDirectionNorthCompare(pos1, pos2);
+		int omni = getOmniDirectionalCompare(pos1, pos2);
+		if (omni != 0) {
+			return omni;
 		}
-		if (this.direction == Direction.EAST) {
-			return getDirectionEastCompare(pos1, pos2);
+
+		if (this.direction == Direction.NORTH || this.direction == Direction.SOUTH) {
+			return getDirectionNorthSouthCompare(pos1, pos2);
 		}
-		if (this.direction == Direction.SOUTH) {
-			return getDirectionSouthCompare(pos1, pos2);
+		if (this.direction == Direction.WEST || this.direction == Direction.EAST) {
+			return getDirectionEastWestCompare(pos1, pos2);
 		}
-		if (this.direction == Direction.WEST) {
-			return getDirectionWestCompare(pos1, pos2);
-		}
-		return -getDirectionNorthCompare(pos1, pos2);
+
+		return 0;
 	}
 
-	public int getDirectionNorthCompare(Position pos1, Position pos2) {
+	public int getOmniDirectionalCompare(Position pos1, Position pos2) {
 		float me = heightCompareLength(pos1);
 		float other = heightCompareLength(pos2);
 
@@ -46,13 +46,17 @@ public class PositionComperator implements Comparator<Position> {
 		// both bodys same vertical height
 
 		// no that are realy object on same height
-		if(pos1.zFraction>pos2.zFraction){
+		if (pos1.zFraction > pos2.zFraction) {
 			return -1;
 		}
-		if(pos1.zFraction<pos2.zFraction){
+		if (pos1.zFraction < pos2.zFraction) {
 			return 1;
 		}
-		
+
+		return 0;
+	}
+
+	public int getDirectionNorthSouthCompare(Position pos1, Position pos2) {
 		if (pos1.x < pos2.x) {
 			return 1;
 		}
@@ -70,18 +74,7 @@ public class PositionComperator implements Comparator<Position> {
 		return 0;
 	}
 
-	public int getDirectionWestCompare(Position pos1, Position pos2) {
-		float me = heightCompareLength(pos1);
-		float other = heightCompareLength(pos2);
-
-		if (me > other) {
-			return -1;
-		}
-		if (me < other) {
-			return 1;
-		}
-
-		// both bodys same vertical height
+	public int getDirectionEastWestCompare(Position pos1, Position pos2) {
 		if (pos1.y < pos2.y) {
 			return 1;
 		}
@@ -99,32 +92,38 @@ public class PositionComperator implements Comparator<Position> {
 		return 0;
 	}
 
-	public int getDirectionSouthCompare(Position pos1, Position pos2) {
-		return -getDirectionNorthCompare(pos1, pos2);
-	}
-
-	public int getDirectionEastCompare(Position pos1, Position pos2) {
-		return -getDirectionWestCompare(pos1, pos2);
-	}
-
 	public float heightCompareLength(Position p) {
-		if (direction == Direction.NORTH || direction == Direction.SOUTH) {
-			return p.x + p.y + fractionLength(p) - p.z - (p.zFraction > 0 ? 1 : 0);
+		int zHeight = -p.z - (p.zFraction > 0 ? 1 : 0);
+		float x = p.fractionLengthX();
+		float y = p.fractionLengthY();
+
+		switch (direction) {
+		case NORTH:
+			return zHeight + (p.x + p.y + x + y);
+		case SOUTH:
+			return zHeight + (-p.x - p.y - x - y);
+		case WEST:
+			return zHeight + (-p.x + p.y - x + y);
+		case EAST:
+			return zHeight + (p.x - p.y + x - y);
+		default:
+			return 0;
 		}
-		if (direction == Direction.EAST || direction == Direction.WEST) {
-			return -p.x + p.y + fractionLength(p);
-		}
-		return p.x + p.y + fractionLength(p);
 	}
 
 	private float fractionLength(Position p) {
-		if (direction == Direction.NORTH || direction == Direction.SOUTH) {
-			return p.fractionLengthX() + p.fractionLengthY();
-		}
-		if (direction == Direction.EAST || direction == Direction.WEST) {
-			return -p.fractionLengthX() + p.fractionLengthY();
-		}
-		return p.fractionLengthX() + p.fractionLengthY();
+		float x = p.fractionLengthX();
+		float y = p.fractionLengthY();
+		boolean northSouth = (direction == Direction.NORTH || direction == Direction.SOUTH);
+		return northSouth ? x + y : -x + y;
+
+		// if (direction == Direction.NORTH || direction == Direction.SOUTH) {
+		// return p.fractionLengthX() + p.fractionLengthY();
+		// }
+		// if (direction == Direction.EAST || direction == Direction.WEST) {
+		// return -p.fractionLengthX() + p.fractionLengthY();
+		// }
+		// return p.fractionLengthX() + p.fractionLengthY();
 	}
 
 }
